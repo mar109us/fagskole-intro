a3b256c6962b7ab629506afdf48ef56bafe30225
ionide <mar109us@gmail.com>
2026-08-12 19:28:09 +0200

    removed css code since its not nessecary

--- 116/116D/main.js
+++ 116/116D/main.js
@@ -1,39 +1,35 @@
-let light = document.getElementById('light')
+let light = document.getElementById("light");
 
-let redLight = document.getElementById('red-light')
-let yellowLight = document.getElementById('yellow-light')
-let greenLight = document.getElementById('green-light')
+let redLight = document.getElementById("red-light");
+let yellowLight = document.getElementById("yellow-light");
+let greenLight = document.getElementById("green-light");
 
-let count = 1
+let count = 1;
 
-light.addEventListener("click",() => {
-   console.log(count)
+light.addEventListener("click", () => {
+   console.log(count);
    if (count === 5) {
-      count = 1
-   }
-   else {
-      count++
+      count = 1;
+   } else {
+      count++;
    }
 
-   checkState(count)
-})
+   checkState(count);
+});
 
 function checkState(currentState) {
    if (currentState === 1) {
-      redLight.style.backgroundColor = "red"
-      yellowLight.style.backgroundColor = "red"
-      greenLight.style.backgroundColor = "red"
+      redLight.style.backgroundColor = "red";
+      yellowLight.style.backgroundColor = "gray";
+      greenLight.style.backgroundColor = "gray";
    }
    if (currentState === 2) {
-      
    }
    if (currentState === 3) {
-      
    }
    if (currentState === 4) {
-      
    }
    if (currentState === 5) {
-      
    }
 }
+checkState(count)
