7c9d4dff948f1debb2ec3d8d1e13599b676baa7c
ionide <mar109us@gmail.com>
2026-08-17 14:16:05 +0200

    trying to change rows on increment

--- emne 1/4 løkker lister/151/151G/main.js
+++ emne 1/4 løkker lister/151/151G/main.js
@@ -9,7 +9,32 @@ function writeSpace() {
 function writeLineBreak() {
    document.getElementById("output").innerText += "\n";
 }
+let maxRows = 4;
+let maxColumns = 8;
 
-for(index = 0; index<10 ; index++) {
-   writeHash()
+let currentRow = 0;
+let currentColumn = 0;
+
+let hash1 = 8;
+function printOut() {
+   for (index = 0; index < maxRows; index++) {
+      printRow();
+      writeLineBreak();
+   }
+}
+printOut();
+
+function printRow() {
+   for (let index = 0; index < maxColumns; index++) {
+      let count = 0;
+      if (count === 0) {
+         writeHash();
+         count++;
+      } else {
+         for (let count = 0; index < count; index++) {
+            writeSpace();
+            console.log("mew")
+         }
+      }
+   }
 }
