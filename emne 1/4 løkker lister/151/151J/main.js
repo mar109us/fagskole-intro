340f5dae7d5934a368669b3a414353bd6f1d6f91
ionide <mar109us@gmail.com>
2026-09-01 22:19:48 +0200

    silly solution to counting words with counting spaces + 1

--- emne 1/4 løkker lister/151/151J/main.js
+++ emne 1/4 løkker lister/151/151J/main.js
@@ -3,21 +3,38 @@
 // Tell antall ord
 // Finn det lengste ordet
 // Finn ord med flest vokaler
+
 const code = " ".charCodeAt(0); // 32
+
 updateView();
 function updateView(data) {
+   initView();
    if (data) {
       document.getElementById("output").innerHTML += `<h1>${data}</h1>`;
-      for (i = 0; i <)
-      document.getElementById("output").innerHTML += `<h1>spaces ${data.String.fromCharCode(32)}</h1>`;
-   } else {
-      initView();
+
+      document.getElementById("output").innerHTML += `
+      <ul>
+         <li>Words: ${countSpaces(data)}</li>
+         <li>Longest word: ${countSpaces(data)}</li>
+      <ul>
+      `;
    }
 }
 
 function initView() {
    return (document.getElementById("output").innerHTML = `
-   <button onclick="initView()">reset</button>
    <input type="text" onchange="updateView(this.value)">
+   <button type="submit" onclick="initView()">reset</button>
    `);
+}
+
+function countSpaces(data) {
+   let count = 0;
+   for (i = 0; i < data.length; i++) {
+      if (data.charCodeAt(i) === 32) {
+         count++;
+      }
+   }
+   let lastCharacterProbablyNotSpace = 1
+   return count + lastCharacterProbablyNotSpace;
 }
