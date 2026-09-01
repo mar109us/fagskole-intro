0af762add9c23fdf3212089a46b3967a5a0240df
ionide <mar109us@gmail.com>
2026-09-01 21:57:17 +0200

    progress 151J

--- emne 1/4 løkker lister/151/151J/main.js
+++ emne 1/4 løkker lister/151/151J/main.js
@@ -1,9 +1,23 @@
+// 151J
 // Les input
 // Tell antall ord
 // Finn det lengste ordet
 // Finn ord med flest vokaler
-
-function view() {
-   document.getElementById("output").innerHTML = `test`;
+const code = " ".charCodeAt(0); // 32
+updateView();
+function updateView(data) {
+   if (data) {
+      document.getElementById("output").innerHTML += `<h1>${data}</h1>`;
+      for (i = 0; i <)
+      document.getElementById("output").innerHTML += `<h1>spaces ${data.String.fromCharCode(32)}</h1>`;
+   } else {
+      initView();
+   }
 }
-view()
+
+function initView() {
+   return (document.getElementById("output").innerHTML = `
+   <button onclick="initView()">reset</button>
+   <input type="text" onchange="updateView(this.value)">
+   `);
+}
