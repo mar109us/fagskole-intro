15609e9778140abd43be256ee8c0f7c7841dacbb
ionide <mar109us@gmail.com>
2026-09-01 20:57:24 +0200

    finished

--- emne 1/4 løkker lister/151/151G/main.js
+++ emne 1/4 løkker lister/151/151G/main.js
@@ -3,21 +3,21 @@ function writeHash() {
 }
 
 function writeSpace() {
-   document.getElementById("output").innerText += " ";
+   document.getElementById("output").innerText +=  "\u00A0";
 }
 
 function writeLineBreak() {
    document.getElementById("output").innerText += "\n";
 }
 let maxRows = 4;
-let maxColumns = 8;
+let maxHash = 8;
 
 let currentRow = 0;
 let currentColumn = 0;
 
-let hash1 = 8;
 function printOut() {
-   for (index = 0; index < maxRows; index++) {
+   for (i = 0; i < maxRows; i++) {
+      console.log(currentRow);
       printRow();
       writeLineBreak();
    }
@@ -25,16 +25,15 @@ function printOut() {
 printOut();
 
 function printRow() {
-   for (let index = 0; index < maxColumns; index++) {
-      let count = 0;
-      if (count === 0) {
-         writeHash();
-         count++;
-      } else {
-         for (let count = 0; index < count; index++) {
-            writeSpace();
-            console.log("mew")
-         }
-      }
+   for (let i = 0; i < currentRow; i++) {
+      writeSpace();
    }
+   for (let i = 0; i < maxHash; i++) {
+      writeHash();
+   }
+   for (let i = 0; i < currentRow; i++) {
+      writeSpace();
+   }
+   currentRow++;
+   maxHash -= 2;
 }
