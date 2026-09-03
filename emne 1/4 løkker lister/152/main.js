4f532a026bd4d65b501e9363dafefc3ba2d4ebea
ionide <mar109us@gmail.com>
2026-09-03 12:53:19 +0200

    finished 152A

--- emne 1/4 løkker lister/152/main.js
+++ emne 1/4 løkker lister/152/main.js
@@ -1,7 +1,34 @@
 /* 
 152 A  
-Lag en funksjon som lager og returnerer en liste 
-med hvert sjuende tall fra 100 til 1000, altså 100, 107, 114 osv.
+funksjon lager array
+samme funksjon returnerer en liste 
+med hvert sjuende tall fra 100 til 1000, 
+100, 107, 114 osv.*/
 
+// const app = document.getElementById("app");
+
+// updateView();
+// function updateView() {
+//    app.innerHTML += everySeven();
+// }
+
+// function everySeven() {
+//    let numbersArray = [];
+//    let numbers = "";
+//    let min = 100;
+//    let max = 1000;
+//    let step = 7
+
+//    for (let i = min; i <= max; i += step) {
+//       numbersArray.push(i);
+//    }
+
+//    for (let i of numbersArray) {
+//       numbers += `${i} `;
+//    }
+//    return numbers;
+// }
+
+/* 
 152 B  
 Lag en funksjon som tar en liste av tall som parameter og som returnerer summen av tallene. */
