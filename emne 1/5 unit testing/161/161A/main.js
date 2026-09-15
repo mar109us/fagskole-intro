7f082da136797c2327fe02aa3591a435afdef9b8
ionide <mar109us@gmail.com>
2026-09-15 12:50:46 +0200

    remove spaces from start

--- emne 1/5 unit testing/161/161A/main.js
+++ emne 1/5 unit testing/161/161A/main.js
@@ -1,3 +1,23 @@
-function add(a, b) {
-  return a + b;
+// function add(a, b) {
+//   return a + b;
+// }
+
+// Create a file named main.js with the function function fixText(txt).
+
+// Trim all leading and trailing whitespace from the input.
+
+// Format the string so the first letter is uppercase and all subsequent letters are lowercase.
+
+// Test-first requirement: Write QUnit tests for the new lowercase rule before updating your function to pass them.
+
+function fixText(text) {
+   let modifyText = text;
+   while (modifyText[0] === " ") {
+      console.log("mew");
+      modifyText = modifyText.slice(1);
+      console.log(modifyText);
+   }
+   
+   return modifyText;
 }
+fixText("12");
--- emne 1/5 unit testing/161/161A/test.js
+++ emne 1/5 unit testing/161/161A/test.js
@@ -1,5 +1,39 @@
-QUnit.module("add", function () {
-   QUnit.test("two numbers", function (assert) {
-      assert.equal(add(1, 2), 3);
-   });
+// QUnit.module("add", function () {
+//    QUnit.test("two numbers", function (assert) {
+//       assert.equal(add(1, 2), 3);
+//    });
+// });
+
+
+
+// Create a file named main.js with the function function fixText(txt).
+
+// Trim all leading and trailing whitespace from the input.
+
+// Format the string so the first letter is uppercase and all subsequent letters are lowercase.
+
+// Test-first requirement: Write QUnit tests for the new lowercase rule before updating your function to pass them.
+
+QUnit.test("fix - 'terje'", function (assert) {
+   const actual = fixText("terje");
+   const expected = "Terje";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("fix - 'terje '", function (assert) {
+   const actual = fixText("terje ");
+   const expected = "Terje";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("fix - ' terje'", function (assert) {
+   const actual = fixText(" terje");
+   const expected = "Terje";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("fix - ' terje '", function (assert) {
+   const actual = fixText(" terje ");
+   const expected = "Terje";
+   assert.equal(actual, expected);
 });
