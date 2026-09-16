3172ea2b0032ffeda6222ffdda94e4f64bf2bf2f
ionide <mar109us@gmail.com>
2026-09-16 10:37:27 +0200

    finished task

--- emne 1/5 unit testing/161/161A/main.js
+++ emne 1/5 unit testing/161/161A/main.js
@@ -1,7 +1,3 @@
-// function add(a, b) {
-//   return a + b;
-// }
-
 // Create a file named main.js with the function function fixText(txt).
 
 // Trim all leading and trailing whitespace from the input.
@@ -11,46 +7,49 @@
 // Test-first requirement: Write QUnit tests for the new lowercase rule before updating your function to pass them.
 
 function fixText(text) {
-   let modifyText = text.toString()
+   if (text === undefined || text === null || onlySpace(text)) {
+      return "No text received";
+   }
+   let modifyText = text.toString();
    modifyText = removeSpaceFromStart(modifyText);
    modifyText = removeSpaceFromEnd(modifyText);
-   modifyText = setToLowercase(modifyText)
-   modifyText = capitalizeFirstLetter(modifyText)
-
-   console.log(modifyText);
+   modifyText = setToLowercase(modifyText);
+   modifyText = capitalizeFirstLetter(modifyText);
    return modifyText;
 }
-fixText("     teRje      ");
+fixText("  1  ");
+
+function onlySpace(text) {
+   for (let i of text) {
+      if (i !== " ") {
+         return false;
+      }
+   }
+   return true;
+}
 
 function removeSpaceFromStart(text) {
-   console.log("REMOVE SPACE FROM START");
-   console.log("before:", text.replaceAll(" ", "(space)"));
    while (text[0] === " ") {
       text = text.slice(1);
    }
-   console.log("after:", text);
    return text;
 }
 
 function removeSpaceFromEnd(text) {
    let lastIndex = text.length - 1;
-   console.log("REMOVE SPACE FROM END");
-   console.log("before:", text.replaceAll(" ", "(space)"));
    while (text[lastIndex] === " ") {
       text = text.slice(0, lastIndex);
-      lastIndex = text.length - 1
+      lastIndex = text.length - 1;
    }
-   console.log("after:", text.replaceAll(" ", "(space)"));
    return text;
 }
 
 function setToLowercase(text) {
-   text = text.toLowerCase()
-   return text
+   text = text.toLowerCase();
+   return text;
 }
 
-function capitalizeFirstLetter(text){
-   text = text[0].toUpperCase() + text.slice(1)
-   return text
+function capitalizeFirstLetter(text) {
+   text = text[0].toUpperCase() + text.slice(1);
+   return text;
 }
-
--- emne 1/5 unit testing/161/161A/test.js
+++ emne 1/5 unit testing/161/161A/test.js
@@ -1,11 +1,3 @@
-// QUnit.module("add", function () {
-//    QUnit.test("two numbers", function (assert) {
-//       assert.equal(add(1, 2), 3);
-//    });
-// });
-
-
-
 // Create a file named main.js with the function function fixText(txt).
 
 // Trim all leading and trailing whitespace from the input.
@@ -14,13 +6,73 @@
 
 // Test-first requirement: Write QUnit tests for the new lowercase rule before updating your function to pass them.
 
-QUnit.test("Uppercase first letter", function (assert) {
+QUnit.test("Null", function (assert) {
+   const actual = fixText(null);
+   const expected = "No text received";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("Undefined", function (assert) {
+   const actual = fixText(undefined);
+   const expected = "No text received";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("Empty", function (assert) {
+   const actual = fixText("");
+   const expected = "No text received";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("Multiple spaces", function (assert) {
+   const actual = fixText("        ");
+   const expected = "No text received";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("Space only", function (assert) {
+   const actual = fixText(" ");
+   const expected = "No text received";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("All uppercase", function (assert) {
+   const actual = fixText("TERJE");
+   const expected = "Terje";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("Random uppercase", function (assert) {
+   const actual = fixText("tErJE");
+   const expected = "Terje";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("Random uppercase with outer spaces", function (assert) {
+   const actual = fixText("    tErJE   ");
+   const expected = "Terje";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("Lowercase first letter", function (assert) {
    const actual = fixText("terje");
    const expected = "Terje";
    assert.equal(actual, expected);
 });
 
-QUnit.test("No space before or after", function (assert) {
+QUnit.test("Space after text", function (assert) {
+   const actual = fixText("terje ");
+   const expected = "Terje";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("Space before text", function (assert) {
+   const actual = fixText(" terje");
+   const expected = "Terje";
+   assert.equal(actual, expected);
+});
+
+QUnit.test("Space after and before text", function (assert) {
    const actual = fixText(" terje ");
    const expected = "Terje";
    assert.equal(actual, expected);
