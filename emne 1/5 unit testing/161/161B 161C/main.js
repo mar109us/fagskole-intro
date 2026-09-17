cc1c494db8b862ba09b0e2186ec3d1c6a14a6aca
ionide <mar109us@gmail.com>
2026-09-17 10:35:51 +0200

    task complete

--- emne 1/5 unit testing/161/161B 161C/main.js
+++ emne 1/5 unit testing/161/161B 161C/main.js
@@ -1,5 +1,28 @@
+//skriv test først
+//test sjekker om epost
+//skriv så funksjon som sjekker om @ er inkludert
+
 function checkEmail(emailToCheck) {
-   if (emailToCheck.includes("@") || emailToCheck.includes(".")){
-      return true;
-   } 
+   let checkFormat = checkSpecialCharacters(emailToCheck);
+   // checkFormat = checkFormat.toString();
+   console.log(checkFormat);
+   if (checkFormat.includes(".@.") || checkFormat.includes(".@") || checkFormat.includes("@.")) return checkFormat;
+   else if (emailToCheck.includes("@") || emailToCheck.includes(".")) return true;
+   else return false;
+}
+checkEmail("as.d@asdcom");
+
+function checkSpecialCharacters(emailToCheck) {
+   let temp = "";
+   for (let i of emailToCheck) {
+      if (i === "." && temp === "") temp += i;
+      if (i === "@" && temp === "") temp += i;
+      if (i === "@" && temp === ".") temp += i;
+      if (i === "." && temp === ".@") temp += i;
+      if (i === "." && temp === "@") temp += i;
+   }
+   console.log(emailToCheck);
+   console.log(temp);
+   if (temp !== "") return temp;
+   else return "";
 }
--- emne 1/5 unit testing/161/161B 161C/test.js
+++ emne 1/5 unit testing/161/161B 161C/test.js
@@ -1,14 +1,30 @@
-//skriv test først
-//test sjekker om epost
-//skriv så funksjon som sjekker om @ er inkludert
-
-QUnit.test("Email has @", function (assert) {
+QUnit.test("Positive: Contains '@'", function (assert) {
    const test = checkEmail("@");
    const expected = true;
    assert.equal(test, expected);
 });
-QUnit.test("Email has .", function (assert) {
+QUnit.test("Negative: No '@'", function (assert) {
+   const test = checkEmail("asd");
+   const expected = false;
+   assert.equal(test, expected);
+});
+QUnit.test("Positive: Contains '.'", function (assert) {
    const test = checkEmail(".");
    const expected = true;
+   assert.equal(test, expected);
+});
+QUnit.test("Negative: No '.'", function (assert) {
+   const test = checkEmail("asd");
+   const expected = false;
+   assert.equal(test, expected);
+});
+QUnit.test("Positive: Contains '.' before '@'", function (assert) {
+   const test = checkEmail("a.sd@asd");
+   const expected = ".@";
+   assert.equal(test, expected);
+});
+QUnit.test("Positive: Contains '.' after '@'", function (assert) {
+   const test = checkEmail("asd@asd.com");
+   const expected = "@.";
    assert.equal(test, expected);
 });
